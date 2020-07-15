47c2734aee2c2f9aa38dfbbf85d698a708fff3b8
mikepadiernos <mike.padiernos@gmail.com>
2020-07-14 21:36:05 -0400

    [FEATURE] concepts / classes

--- src/15_classes.py
+++ src/15_classes.py
@@ -40,15 +40,15 @@ class Geocache(Waypoint):
 
 # Make a new waypoint and print it out: "Catacombs", 41.70505, -121.51521
 
-waypoint = Waypoint("Catacombs", 41.70505, -121.51521)
+wp = Waypoint("Catacombs", 41.70505, -121.51521)
 
 # Without changing the following line, how can you make it print into something
 # more human-readable? Hint: Look up the `object.__str__` method
-print(waypoint)
+print(wp)
 
 # Make a new geocache "Newberry Views", diff 1.5, size 2, 44.052137, -121.41556
 
-geocache = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
+gc = Geocache("Newberry Views", 1.5, 2, 44.052137, -121.41556)
 
 # Print it--also make this print more nicely
-print(geocache)
+print(gc)
